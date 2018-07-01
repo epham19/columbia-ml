73a3dad2fb77686883011041219f582b3b0fec00
epham19 <36887941+epham19@users.noreply.github.com>
2018-07-01 23:59:20 +1000

    structure script logic

--- hw1_regression.py
+++ hw1_regression.py
@@ -1,20 +1,16 @@
 import numpy as np
 import sys
 
-lambda_input = int(sys.argv[1])
-sigma2_input = float(sys.argv[2])
-X_train = np.genfromtxt(sys.argv[3], delimiter=",")
-y_train = np.genfromtxt(sys.argv[4])
-X_test = np.genfromtxt(sys.argv[5], delimiter=",")
+
 
 # Solution for Part 1
-def part1():
+def part1(lambda_input,sigma2_input):
     # Input : Arguments to the function
     # Return : wRR, Final list of values to write in the file
     pass
 
 wRR = part1()  # Assuming wRR is returned from the function
-np.savetxt("wRR_" + str(lambda_input) + ".csv", wRR, delimiter="\n") # write output to file
+np.savetxt("wRR_" + str(lambda_input) + ".csv", wRR, delimiter="\n")  # write output to file
 
 
 # Solution for Part 2
@@ -23,5 +19,13 @@ def part2():
     # Return : active, Final list of values to write in the file
     pass
 
+def main():
+    lambda_input = int(sys.argv[1])
+    sigma2_input = float(sys.argv[2])
+    X_train = np.genfromtxt(sys.argv[3], delimiter=",")
+    y_train = np.genfromtxt(sys.argv[4])
+    X_test = np.genfromtxt(sys.argv[5], delimiter=",")
+
+
 active = part2()  # Assuming active is returned from the function
-np.savetxt("active_" + str(lambda_input) + "_" + str(int(sigma2_input)) + ".csv", active, delimiter=",") # write output to file
+np.savetxt("active_" + str(lambda_input) + "_" + str(int(sigma2_input)) + ".csv", active, delimiter=",")  # write output to file
