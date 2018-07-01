b080f7b1bdd72ec7f0094e77b649316092382b27
epham19 <36887941+epham19@users.noreply.github.com>
2018-07-01 21:26:18 +1000

    initial solutions template

---
+++ hw1_regression.py
@@ -0,0 +1,27 @@
+import numpy as np
+import sys
+
+lambda_input = int(sys.argv[1])
+sigma2_input = float(sys.argv[2])
+X_train = np.genfromtxt(sys.argv[3], delimiter = ",")
+y_train = np.genfromtxt(sys.argv[4])
+X_test = np.genfromtxt(sys.argv[5], delimiter = ",")
+
+## Solution for Part 1
+def part1():
+    ## Input : Arguments to the function
+    ## Return : wRR, Final list of values to write in the file
+    pass
+
+wRR = part1()  # Assuming wRR is returned from the function
+np.savetxt("wRR_" + str(lambda_input) + ".csv", wRR, delimiter="\n") # write output to file
+
+
+## Solution for Part 2
+def part2():
+    ## Input : Arguments to the function
+    ## Return : active, Final list of values to write in the file
+    pass
+
+active = part2()  # Assuming active is returned from the function
+np.savetxt("active_" + str(lambda_input) + "_" + str(int(sigma2_input)) + ".csv", active, delimiter=",") # write output to file
